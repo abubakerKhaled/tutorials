e347df49a736eadead125646ee034d4798308a53
Abubakr <110747776+abubakerKhaled@users.noreply.github.com>
2025-02-12 15:04:40 +0200

    build the real estate property model

---
+++ estate/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Tutorial",
+    'category': 'Tutorials/Real Estate',
+    'description': """
+        Looking for Properties then we have the Real Estate app
+    """,
+    'application': True,
+    # data files always loaded at installation
+    # 'data': [
+    #     'views/mymodule_view.xml',
+    # ],
+    # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+}
